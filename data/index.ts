a7737dffe90f3fb6854dcca570ee860491aeed39
khushab <khushabalam7@gmail.com>
2024-09-02 18:55:58 +0530

    Project setup

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Cutting-Edge AI Experience",
+    des: "Explore Brainwave, where intuitive UX meets advanced AI for a seamless user experience.",
+    img: "/brainwave.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Revolutionary Banking Interface",
+    des: "Hoobank offers a visually stunning UI, setting a new benchmark in digital banking.",
+    img: "/hoobank.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Immersive Digital Realms",
+    des: "Discover Metaverse, a sleek UI crafted for a captivating and futuristic experience.",
+    img: "/metaverse.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Culinary Experience Redefined",
+    des: "Grilli combines exceptional UI and UX to deliver a modern, user-centric dining experience.",
+    img: "/grilli.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Faiz was an absolute game-changer. His attention to detail and commitment to excellence are unmatched. Every aspect of our project was handled with care, and the results speak for themselves. If you’re looking for someone who delivers beyond expectations, Faiz is your guy.",
+    name: "Michael Carter",
+    title: "CEO of CarterTech Solutions",
+  },
+  {
+    quote:
+      "Working with Faiz was a seamless experience. His expertise and passion for design are evident in every interaction. From the initial concept to the final product, Faiz ensured that everything aligned perfectly with our vision. Highly recommended for anyone serious about quality.",
+    name: "Samantha Reid",
+    title: "Creative Director at Innovate Studio",
+  },
+  {
+    quote:
+      "Faiz brought our ideas to life with a level of creativity and precision that’s hard to find. His approach is both innovative and grounded, making the entire process smooth and enjoyable. If you want someone who truly understands your needs, Faiz is the perfect choice.",
+    name: "Daniel Thompson",
+    title: "Co-Founder of BrightVision Media",
+  },
+  {
+    quote:
+      "Partnering with Faiz was one of the best decisions we made. His professionalism and skill set are top-notch, and he consistently delivers results that exceed expectations. For anyone looking to enhance their online presence, Faiz is the expert you need.",
+    name: "Emily Foster",
+    title: "Marketing Manager at NextGen Enterprises",
+  },
+  {
+    quote:
+      "Faiz's expertise transformed our vision into reality. His ability to understand our needs and deliver a polished, professional product was impressive. From start to finish, Faiz was dedicated to ensuring our satisfaction. If you’re looking for someone who truly cares about your success, Faiz is the one to work with.",
+    name: "Jonathan Blake",
+    title: "Founder of Apex Digital Solutions",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Maverick Swing Trader",
+    desc: "Expert in identifying and capitalizing on short-term trading opportunities.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Lead Frontend Developer",
+    desc: "Crafting responsive, user-friendly interfaces with cutting-edge technology.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Dynamo",
+    desc: "Delivering versatile, high-quality results across various projects and industries.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Design Virtuoso",
+    desc: "Creating visually stunning graphics that tell compelling stories.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
